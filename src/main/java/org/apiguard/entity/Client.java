e35926c4da73231648e1ae5cdcc8717a073fbc8d
Stanley Lam <slam@slam014575.starbucks.net>
2017-04-25 13:01:19 -0700

    added http signature

--- src/main/java/org/apiguard/entity/Client.java
+++
@@ -1,5 +0,0 @@
-package org.apiguard.entity;
-
-public interface Client extends Base {
-	public String getClientId();
-}
--- src/main/java/org/apiguard/entity/KeyAuth.java
+++
@@ -1,9 +0,0 @@
-package org.apiguard.entity;
-
-public interface KeyAuth extends Base {
-	public String getClientId();
-
-	public String getKey();
-
-	public String getReqUri();
-}
